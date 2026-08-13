cb7158cfbc8a6ab0340a10c1d3c193d4fb1ba58a
Nathee (Juno) Thiensirisak <junothiensirisak@gmail.com>
2026-08-13 13:47:29 -0400

    Add docstring to get_data function
    Added docstring to the get_data function to explain its purpose, return type, and provide usage examples.

--- UFCData/data.py
+++ UFCData/data.py
@@ -3,6 +3,20 @@ import pickle
 import logging
 
 def get_data():
+    """
+    Obtains all UFC related data
+
+    Returns
+    dict
+        Dictionary containing UFC data as pandas DataFrames.
+        Individual DataFrames can be accessed using their
+        corresponding dictionary keys.
+
+    Examples
+    --------
+    >>> data = get_data()
+    >>> past_events = data["past_events"].copy()
+    """
 
     logging.getLogger("huggingface_hub").setLevel(logging.ERROR)
     
