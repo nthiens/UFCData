4022c228a985a5a68f4e136ff1dab08cff4a4b8d
Nathee (Juno) Thiensirisak <junothiensirisak@gmail.com>
2026-08-27 20:03:19 -0400

    Refactor imports in __init__.py for evaluation and plots

--- UFCData/__init__.py
+++ UFCData/__init__.py
@@ -3,4 +3,5 @@ from .search import search
 from .ratings import get_elo
 from .fighter import get_fighter_history, get_fighter_statistic
 from .helper import convert_odds, convert_gender, convert_weight, mirror
-from .evaluation import train_test_split, get_expanding_window, plot_cv, plot_test
+from .evaluation import train_test_split, get_expanding_window
+from .plots import plot_fight_graph, plot_body, plot_line_graph, plot_bar_graph
